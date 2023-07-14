cae801af2c036490077b9b8dcc6feb563123614e
zubibli <zubibli@rediffmail.com>
2023-07-13 23:57:27 -0700

    Size 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Start
+*
+* Description: This output the size of different nomenclature"
+* Return: This returns 0
+*/
+int main(void)
+{
+printf("Size of a char: %i byte(s)\n, sizeof(char));
+printf("Size of an int: %i bytes(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n, sizeof(long int));
+printf("@Size of a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a float: %i byte(s0\n, sizeof(float));
+return (0);
+}
