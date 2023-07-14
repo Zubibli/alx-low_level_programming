d237c2975ca2fa3727a08427b0f5d44723eb396a
zubibli <zubibli@rediffmail.com>
2023-07-13 21:40:28 -0700

    For the four inputs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+
+/**
+ * main - Entry point
+ *
+ *Description: This prints out using puts
+ *Return: This retuns 0
+ */
+
+
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
