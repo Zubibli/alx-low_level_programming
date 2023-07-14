0b836032305fdff5b18784ad1bf0e1f22de7b86f
zubibli <zubibli@rediffmail.com>
2023-07-14 03:06:29 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 
 /**
 * main - Entry point
-* Desceiption: this will make use of the write function
-* Return: returns error 1
+* Description: the code will make use of the write function
+* Return: return error 1
 */
 
 int main(void)
@@ -12,4 +12,4 @@ char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 write(1, str, 59);
 return (1);
-} 
+}
