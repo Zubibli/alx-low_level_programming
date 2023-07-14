4256dec309240fc797086709455f152e070debf6
zubibli <zubibli@rediffmail.com>
2023-07-13 22:06:40 -0700

    Print f 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - This prints out a value
+* Return: This returns 0
+*/
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece art,\n");
+  return (0);
+}
