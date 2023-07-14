8da1c49ea2123b11f07f9b666ddd320263b76769
zubibli <zubibli@rediffmail.com>
2023-07-13 21:59:47 -0700

    For the four puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,9 +7,9 @@
 *
 *Return: This retuns 0
 */
-
 int main(void)
+
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
