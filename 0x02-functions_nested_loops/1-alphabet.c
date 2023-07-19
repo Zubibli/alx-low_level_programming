2554982b26aad97069a5d4d65dbe2f7b8bbf2e9b
zubibli <zubibli@rediffmail.com>
2023-07-19 16:12:35 -0700

    1 alphabet c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prototype function
+ * Description: This prints out alphabet from a-z
+ * in lower case
+ * Return: this returns void
+ * */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
