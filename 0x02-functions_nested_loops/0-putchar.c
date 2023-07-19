4aa2b5edadf3f1033102773aa4301642a8c29946
zubibli <zubibli@rediffmail.com>
2023-07-19 15:54:30 -0700

    0 putchar c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: This source code is to print a alue
+ * _putchar
+ * Return: This returns 0(success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
