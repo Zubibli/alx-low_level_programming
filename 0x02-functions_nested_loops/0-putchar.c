4c9958d2688ac2b4633599b334c4f1b3f0ab6a8d
zubibli <zubibli@rediffmail.com>
2023-07-20 00:16:35 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,14 +3,14 @@
 
 /**
  * main - Entry point
- * Description: This source code is to print a alue
+ * Description: This source code is to print out a value
  * _putchar
- * Return: This returns 0(success)
+ * Return: This returns 0 (success)
  */
 
-int main(void)
+int man(void)
 {
-	_putchar('_');
+	-putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
