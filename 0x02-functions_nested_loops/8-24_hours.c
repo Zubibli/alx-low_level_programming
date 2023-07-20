26904e6be085c330c96049ca0806421981b65a63
zubibli <zubibli@rediffmail.com>
2023-07-20 06:57:05 -0700

    8 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - this function prints out time
+ * Description: This function prints out time for
+ * 00:00 - 23-59
+ * Return: returns void
+ */
+
+void jack_bauer(void)
+{
+	int hours = 0;
+	int seconds = 0;
+
+	while (hours < 24)
+	{
+		seconds = 0;
+		while (seconds < 60)
+		{
+			if (hours < 10)
+			{
+				printf("0");
+				printf("%i", hours);
+			}
+			else
+			{
+				printf("%i", hours);
+			}
+
+			printf(":");
+
+			if (seconds < 10)
+			{
+				printf("0");
+				printf("%i", seconds);
+			}
+			else
+			{
+				printf("%i", seconds);
+			}
+			pritf("\n");
+			seconds++;
+		}
+		hours++;
+	}
