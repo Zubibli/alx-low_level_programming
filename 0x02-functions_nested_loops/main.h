dde26b69e1ce260ed0e93801dd034ba22c557e2c
zubibli <zubibli@rediffmail.com>
2023-07-20 00:29:55 -0700

    0 putchar c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,36 @@
+#ifndef main_h
+#define main_h
+
+/**
+ * function prototype for _putcher
+ */
+
+int _puchart(char c);
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+void add_natural(void0;
+
+#endif
