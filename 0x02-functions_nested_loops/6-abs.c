fee343786f6ab098fd702135bc649c24d6bdcc1c
zubibli <zubibli@rediffmail.com>
2023-07-20 06:10:45 -0700

    6 abs c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - this function
+ * Description: takes the parameter and return the absolute of it
+ * @n: parameter to return absolute
+ * Return: returns the absolute of the parameter
+ */
+
+int _abs(int n)
+{
+	int absolute = abs(n);
+
+	return (absolute);
+}
