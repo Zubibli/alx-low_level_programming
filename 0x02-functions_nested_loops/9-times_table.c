f68ad91c6d20e3b6b43883a2493f2fe822235488
zubibli <zubibli@rediffmail.com>
2023-07-20 07:28:12 -0700

    9 times table c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - function display a time tabele
+ * Description: This function prints out a 9 times table
+ * Return: returns void
+ */
+void times_table(void)
+{
+	int num1 = 0;
+	int num2;
+
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
+			int multiply = num1 * num2;
+			
+			if (num2 > 0)
+			{
+				
+				if (multipy < 10)
+				{
+					printf(", %i", multiply);
+				}
+				else
+				{
+					printf(", %i", multiply);
+				}
+			}
+			else
+			{
+				printf("%i", multiply);
+			}
+			num2++;
+		}
+		printf("\n");
+		num1++;
+	}
+}
