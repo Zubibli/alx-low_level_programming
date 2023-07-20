e968f182c8d51a6cf962fd6d0b7ce3a8c04c4f87
zubibli <zubibli@rediffmail.com>
2023-07-20 08:38:00 -0700

    10 add c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - takes two parameter
+ * @num1: parameter 1
+ * @num2: parameter 2
+ *
+ * Description: The function takes the parameter and prints out
+ * the addition of the two
+ *
+ * Return: returns the addition of the two int
+ */
+
+int add(int num1, int num2)
+{
+	int total;
+
+	total = num1 + num2;
+	return (total);
+}
+
