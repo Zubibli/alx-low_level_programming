0c8d35b2093d9505bbd9692590c47d2794dc919a
zubibli <zubibli@rediffmail.com>
2023-07-20 01:25:11 -0700

    2 print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this function
+ * Description: This prints out a-z lower case
+ * and also in multiple of 10
+ * Returns: retuns void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = o; i < 10; i++)
+	{
+		char letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
+}
