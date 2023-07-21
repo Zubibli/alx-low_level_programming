7394d5b72f842fcde9a6037de953dbcae6f18a2e
zubibli <zubibli@rediffmail.com>
2023-07-21 06:14:16 -0700

    10 print triangle c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle - takes a parameter
+ * @size: parameter
+ * Description: Where @size is the size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j, no = size - 1;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j < no)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			no--;
+			-putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
