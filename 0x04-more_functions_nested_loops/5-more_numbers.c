cd79554985687232ed97998610c4cca0993bac70
zubibli <zubibli@rediffmail.com>
2023-07-21 02:38:42 -0700

    5 more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prints numbers
+ * Description: This function prints out number
+ * from 0 - 14 multiple times
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int times;
+
+	for (times = 0; times < 10; times++)
+	{
+		int nu ber = 0;
+
+		while (number <= 14)
+		{
+			if (number > 9)
+			{
+				_putchar(number / 10 + '0'0);
+			}
+			_putchar(number % 10 + '0'0);
+		}
+		_putchar('\n');
+	}
+}
