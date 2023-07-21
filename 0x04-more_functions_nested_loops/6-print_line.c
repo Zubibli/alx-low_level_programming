2c6f30dfb2b099fc0d8495168752514a570d4bc8
zubibli <zubibli@rediffmail.com>
2023-07-21 04:46:52 -0700

    6 print line c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - takes a parameter
+ * @n: parameter
+ * Description: this function takes a parameter @n and print
+ * a line according to number given in the parameter
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n')
+}
