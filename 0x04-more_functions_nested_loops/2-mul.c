0fd821a604410811cc45bc58c0f013fcfa186b91
zubibli <zubibli@rediffmail.com>
2023-07-20 21:54:50 -0700

    2 mul c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * mul - takes a two parameter
+ * @a: parameter
+ * @b: parameter
+ * Description: This function takes a parameter @a
+ * and @b then multiply
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
