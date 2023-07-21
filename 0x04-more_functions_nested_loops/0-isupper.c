86bb6124b6571cbe2eb47fc6de7f01e46c8542ef
zubibli <zubibli@rediffmail.com>
2023-07-20 21:24:49 -0700

    0 isupper c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _iupper - a function that checks uppercase
+ * @c: character to check
+ * Return: 1 success
+ */
+
+int _isupper(int c)
+{
+	if isupper(c)
+		return (1);
+	else
+		return (0);
+}
