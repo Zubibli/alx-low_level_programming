37c16527f21e493439c972f476952cf7726bf981
zubibli <zubibli@rediffmail.com>
2023-07-21 05:04:12 -0700

    7 print diagonal c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - takes a parameter
+ * @n: parameter
+ * Description: This function takes a parameter @n
+ * and print \\ according to the number @n is 
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
