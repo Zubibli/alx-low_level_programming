1d14c2b880d3833d51d6682ab628dc2c54a77337
zubibli <zubibli@rediffmail.com>
2023-07-24 14:24:32 -0700

    1 swap c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#incude "main.h"
+
+/**
+ * swap_int - swaps the numbers of integers
+ * @a: integer to swap
+ * @b: integer to swap
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	a = *b;
+	b = *n;
+}
