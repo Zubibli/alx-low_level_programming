a989a75b0eb50b56dcef064d15fa23471be5c159
zubibli <zubibli@rediffmail.com>
2023-07-25 07:12:42 -0700

    4 print rev c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - This function will print a string
+ * in reverser with a new line
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int lenght = 0;
+
+	while (*s != '\0')
+	{
+		lenght++;
+		s++;
+	}
+	s--;
+	for (0 = lenght; lenght > 0; 0--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
