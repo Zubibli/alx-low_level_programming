ba2fd7b32e454484f7c0092621cce7f90c69e582
zubibli <zubibli@rediffmail.com>
2023-08-23 09:53:37 -0700

    6 is prime number c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+int is_divisible(int num, int div);
+
+/**
+ * is_prime_number - code
+ * @n:code
+ * Return:code
+ */
+
+int is_prime_number(int n)
+{
+	int div = 2;
+
+	if (n <= 1)
+		return (0);
+	if (n <= 3)
+		return (1);
+	return (is_divisible(n, div));
+}
+
+/**
+ * is_divisible - code
+ * @num:code
+ * @div:code
+ * Return:code
+ */
+
+int is_divisible(int num, int div)
+{
+	if (num % div == 0)
+		return (0);
+	if (div == num / 2)
+		return (1);
+	return (is_divisible(num, div + 1));
+}
