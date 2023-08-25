67fb0be1a893e8c36c9a3ae36a04ce213dd69017
zubibli <zubibli@rediffmail.com>
2023-08-25 15:14:36 -0700

    100 first c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,9 +2,12 @@
 
 void __attribute__((constructor)) tortoise(void);
 /**
- * totoise - prints a string before the main function is executed
+ * tortoise - constructor function
+ * tortoise - prints a string before the main function is executed
+ * Description: This is a constructor function that is executed automatically
+ * before the main function when the programme starts.
  */
-void tortoise(void)
+void tortoise(void)/* automatic execution*/
 {
 	printf("you are beat! and yet, you must allow,\n"
 			"i bore my house upon my back!\n");
