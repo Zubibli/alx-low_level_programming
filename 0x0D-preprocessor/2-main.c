42662a2b18d8c3b960698a6f80fb1a774962558f
zubibli <zubibli@rediffmail.com>
2023-08-26 03:33:40 -0700

    2 main c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always for success
+ */
+
+int main(void) /*prints the name of a file, followed by a new line*/
+{
+	printf("% \n, __FILE__);
+	return (1);
+}
